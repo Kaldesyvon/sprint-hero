daeb24c9ff5f0c5af641bc08ad8cb59357251f27
Martin <martinnovysedlak99@gmail.com>
2024-06-13 21:39:29 +0200

    changed order

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
+
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
-    databaseURL: process.env.DATABASE_URL
 };
 
 const app = initializeApp(firebaseConfig);
